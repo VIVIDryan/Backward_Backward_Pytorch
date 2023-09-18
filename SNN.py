973dcb311cbb789445a24fad8128cfbe5cee8374
TravisChen <cxqlove1999@outlook.com>
2023-09-18 20:40:06 +0800

    Add new way of SNN

--- SNN.py
+++ SNN.py
@@ -8,8 +8,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # dataloader arguments
-batch_size = 128
-data_path='/home/datasets/SNN/'
+batch_size = 256
+data_path = '/home/datasets/SNN/'
 
 dtype = torch.float
 DEVICE = torch.device('cuda:2')
@@ -24,18 +24,38 @@ transform = transforms.Compose([
 mnist_train = datasets.MNIST(data_path, train=True, download=False, transform=transform)
 mnist_test = datasets.MNIST(data_path, train=False, download=False, transform=transform)
 
-
 train_loader = DataLoader(mnist_train, batch_size=batch_size, shuffle=True, drop_last=True)
-test_loader = DataLoader(mnist_test, batch_size=batch_size, shuffle=True, drop_last=True)
+test_loader = DataLoader(mnist_test, batch_size=batch_size, shuffle=True, drop_last=False)
 
 # Network Architecture
-num_inputs = 28*28
+num_inputs = 28 * 28
 num_hidden = 1000
 num_outputs = 10
 
 # Temporal Dynamics
 num_steps = 25
 beta = 0.95
+
+def print_batch_accuracy(data, targets, train=False):
+    output, _ = net(data.view(batch_size, -1))
+    _, idx = output.sum(dim=0).max(1)
+    acc = np.mean((targets == idx).detach().cpu().numpy())
+
+    if train:
+        print(f"Train set accuracy for a single minibatch: {acc*100:.2f}%")
+    else:
+        print(f"Test set accuracy for a single minibatch: {acc*100:.2f}%")
+
+def train_printer(
+    data, targets, epoch,
+    counter, iter_counter,
+        loss_hist, test_loss_hist, test_data, test_targets):
+    print(f"Epoch {epoch}, Iteration {iter_counter}")
+    print(f"Train Set Loss: {loss_hist[counter]:.2f}")
+    print(f"Test Set Loss: {test_loss_hist[counter]:.2f}")
+    print_batch_accuracy(data, targets, train=True)
+    print_batch_accuracy(test_data, test_targets, train=False)
+    print("\n")
 
 # Define Network
 class SNNNet(nn.Module):
@@ -67,43 +87,37 @@ class SNNNet(nn.Module):
             mem2_rec.append(mem2)
 
         return torch.stack(spk2_rec, dim=0), torch.stack(mem2_rec, dim=0)
-        
 
+# 准确度计算
+def compute_accuracy(data_loader):
+    total = 0
+    correct = 0
 
-def print_batch_accuracy(data, targets, train=False):
-    output, _ = net(data.view(batch_size, -1))
-    _, idx = output.sum(dim=0).max(1)
-    acc = np.mean((targets == idx).detach().cpu().numpy())
+    with torch.no_grad():
+        net.eval()
+        for data, targets in data_loader:
+            data = data.to(DEVICE)
+            targets = targets.to(DEVICE)
+            test_spk, _ = net(data.view(data.size(0), -1))
+            _, predicted = test_spk.sum(dim=0).max(1)
+            total += targets.size(0)
+            correct += (predicted == targets).sum().item()
 
-    if train:
-        print(f"Train set accuracy for a single minibatch: {acc*100:.2f}%")
-    else:
-        print(f"Test set accuracy for a single minibatch: {acc*100:.2f}%")
-        
-def train_printer(
-    data, targets, epoch,
-    counter, iter_counter,
-        loss_hist, test_loss_hist, test_data, test_targets):
-    print(f"Epoch {epoch}, Iteration {iter_counter}")
-    print(f"Train Set Loss: {loss_hist[counter]:.2f}")
-    print(f"Test Set Loss: {test_loss_hist[counter]:.2f}")
-    print_batch_accuracy(data, targets, train=True)
-    print_batch_accuracy(test_data, test_targets, train=False)
-    print("\n")
-    
+    accuracy = 100 * correct / total
+    return accuracy
+
 # Load the network onto CUDA if available
 net = SNNNet().to(DEVICE)
 loss = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(net.parameters(), lr=5e-4, betas=(0.9, 0.999))
 
-
-num_epochs = 1
+num_epochs = 50 
 loss_hist = []
 test_loss_hist = []
-counter = 0
+train_accuracy_hist = []  # 用于存储训练精度
+test_accuracy_hist = []   # 用于存储测试精度
+train_loss_hist = []      # 用于存储训练损失
 
-
-# Outer training loop
 for epoch in range(num_epochs):
     iter_counter = 0
     train_batch = iter(train_loader)
@@ -117,73 +131,77 @@ for epoch in range(num_epochs):
         net.train()
         spk_rec, mem_rec = net(data.view(batch_size, -1))
 
-        # initialize the loss & sum over time
+        # 初始化损失并在时间上累加
         loss_val = torch.zeros((1), dtype=dtype, device=DEVICE)
         for step in range(num_steps):
             loss_val += loss(mem_rec[step], targets)
 
-        # Gradient calculation + weight update
+        # 梯度计算 + 权重更新
         optimizer.zero_grad()
         loss_val.backward()
         optimizer.step()
 
-        # Store loss history for future plotting
+        # 存储损失历史以供后续绘图
         loss_hist.append(loss_val.item())
+        train_loss_hist.append(loss_val.item())  # 记录训练损失
 
-        # Test set
+        # 测试集
         with torch.no_grad():
             net.eval()
             test_data, test_targets = next(iter(test_loader))
             test_data = test_data.to(DEVICE)
             test_targets = test_targets.to(DEVICE)
 
-            # Test set forward pass
+            # 测试集前向传播
             test_spk, test_mem = net(test_data.view(batch_size, -1))
 
-            # Test set loss
+            # 测试集损失
             test_loss = torch.zeros((1), dtype=dtype, device=DEVICE)
             for step in range(num_steps):
                 test_loss += loss(test_mem[step], test_targets)
             test_loss_hist.append(test_loss.item())
 
-            # Print train/test loss/accuracy
-            if counter % 50 == 0:
+            # 打印训练/测试损失/精度
+            if iter_counter % 50 == 0:
                 train_printer(
                     data, targets, epoch,
-                    counter, iter_counter,
+                    iter_counter, iter_counter,
                     loss_hist, test_loss_hist,
                     test_data, test_targets)
-            counter += 1
-            iter_counter +=1
+            iter_counter += 1
 
-fig = plt.figure(facecolor="w", figsize=(10, 5))
-plt.plot(loss_hist)
-plt.plot(test_loss_hist)
-plt.title("Loss Curves")
-plt.legend(["Train Loss", "Test Loss"])
-plt.xlabel("Iteration")
-plt.ylabel("Loss")
-plt.show()
+    # 在每个epoch结束后计算并存储训练和测试精度
+    train_accuracy = compute_accuracy(train_loader)
+    test_accuracy = compute_accuracy(test_loader)
+    train_accuracy_hist.append(train_accuracy)
+    test_accuracy_hist.append(test_accuracy)
+    print(f"Epoch {epoch}, Train Set Accuracy: {train_accuracy:.2f}%")
+    print(f"Epoch {epoch}, Test Set Accuracy: {test_accuracy:.2f}%")
+    print("\n")
 
-total = 0
-correct = 0
+# 绘制损失曲线和精度曲线
+fig, axes = plt.subplots(1, 3, figsize=(18, 5))
+axes[0].plot(loss_hist)
+axes[0].plot(test_loss_hist)
+axes[0].set_title("Loss Curves")
+axes[0].legend(["Train Loss", "Test Loss"])
+axes[0].set_xlabel("Iteration")
+axes[0].set_ylabel("Loss")
 
-# drop_last switched to False to keep all samples
-test_loader = DataLoader(mnist_test, batch_size=batch_size, shuffle=True, drop_last=False)
+axes[1].plot(train_accuracy_hist)
+axes[1].plot(test_accuracy_hist)
+axes[1].set_title("Accuracy Curves")
+axes[1].legend(["Train Accuracy", "Test Accuracy"])
+axes[1].set_xlabel("Epoch")
+axes[1].set_ylabel("Accuracy (%)")
 
-with torch.no_grad():
-  net.eval()
-  for data, targets in test_loader:
-    data = data.to(DEVICE)
-    targets = targets.to(DEVICE)
-    
-    # forward pass
-    test_spk, _ = net(data.view(data.size(0), -1))
+axes[2].plot(train_loss_hist)
+axes[2].set_title("Train Loss")
+axes[2].set_xlabel("Iteration")
+axes[2].set_ylabel("Loss")
 
-    # calculate total accuracy
-    _, predicted = test_spk.sum(dim=0).max(1)
-    total += targets.size(0)
-    correct += (predicted == targets).sum().item()
+plt.savefig('imgs/snn.png')
 
-print(f"Total correctly classified test set images: {correct}/{total}")
-print(f"Test Set Accuracy: {100 * correct / total:.2f}%")
+# 打印最终的测试精度
+final_test_accuracy = compute_accuracy(test_loader)
+print(f"Final Test Set Accuracy: {final_test_accuracy:.2f}%")
